3165729eb987c7f0ecfdb9e1fa329998a67af5d8
toyayotta-spend <toyayotta@gmail.com>
2026-09-02 20:59:39 +0800

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,19 @@
+/**
+ * Documentation Comment
+ */
+
+public class day01 {
+
+    public static void main(String[] args) {
+
+        // Single-Line Comment
+        System.out.println("Halo Dunia");
+
+        /*
+         * Multi-Line Comment
+         * Used to write long explanations or disable code blocks.
+         */
+
+        System.out.println("Hello World");
+    }
+}
