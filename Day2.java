4e6b57a213082e362ced64485cf6c1d184847b5d
toyayotta-spend <toyayotta@gmail.com>
2026-09-03 10:17:33 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,13 @@
+public class Biodata {
+    public static void main(String[] args) {
+
+        System.out.println("=== BIODATA DIRI ===");
+        System.out.println("Nama : Artika Dwi Putri");
+
+        System.out.print("NIM  : ");
+        System.out.print("D0226345\n");
+
+        System.out.printf("Kelas : %s%n", "D");
+        System.out.printf("Hobi  : %s%n", "Bermain Game");
+    }
+}
