b738ad1a7a05f7a6317d7f8eb7a736172885c877
toyayotta-spend <toyayotta@gmail.com>
2026-09-04 09:30:33 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,30 @@
+public class Day2 {
+    public static void main(String[] args) {
+
+        System.out.println("Nama\t: Artika Dwi Putri");
+        System.out.println("NIM\t: D0226345");
+        System.out.println("Kelas\t: D");
+        System.out.println("Hobi\t: Bermain Game");
+
+        System.out.println("\n=== Contoh Escape Sequence ===");
+
+        System.out.println("1. \\n : Membuat baris baru");
+        System.out.println("Halo\nDunia");
+
+        System.out.println("2. \\t : Membuat tab");
+        System.out.println("Nama\tArtika");
+
+        System.out.println("3. \\\" : Menampilkan tanda kutip");
+        System.out.println("Dia berkata, \"Halo!\"");
+
+        System.out.println("4. \\\\ : Menampilkan backslash");
+        System.out.println("C:\\Users\\Artika");
+
+        System.out.println("5. \\b : Backspace");
+        System.out.println("ABC\bD");
+
+        System.out.println("6. \\r : Carriage return");
+        System.out.println("12345\rABC");
+
+    }
+}
