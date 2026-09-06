a92ae3f191ec7c5e251a2a669fabadee83e0db7e
toyayotta-spend <toyayotta@gmail.com>
2026-09-06 08:37:33 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,28 @@
+public class Day5 {
+    public static void main(String[] args) {
+
+        double tinggiBadan = 165.5;
+        float beratBadan = 55.5f;
+
+        System.out.println(
+            "Nilai dari tipe data double adalah: " + tinggiBadan);
+        System.out.println(
+            "Nilai dari tipe data float adalah: " + beratBadan + "\n");
+
+        System.out.println(
+            "========= Kumpulan Nilai =========");
+
+        System.out.println(
+            "Nilai Minimalnya double adalah: " + Double.MIN_VALUE);
+        System.out.println(
+            "Nilai Maksimalnya double adalah: " + Double.MAX_VALUE);
+
+        System.out.println(
+            "Nilai Minimalnya float adalah: " + Float.MIN_VALUE);
+        System.out.println(
+            "Nilai Maksimalnya float adalah: " + Float.MAX_VALUE);
+
+        System.out.println(
+            "==================================");
+    }
+}
