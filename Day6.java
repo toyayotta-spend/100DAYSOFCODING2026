ec502ac1e424c5928a9c3804b184d4054ab1cadb
toyayotta-spend <toyayotta@gmail.com>
2026-09-07 19:23:06 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,28 @@
+public class Day6 {
+    public static void main(String[] args) {
+
+        char jenisKelamin = 'P';
+        boolean mahasiswa = true;
+
+        System.out.println(
+            "Nilai dari tipe data char adalah: " + jenisKelamin);
+        System.out.println(
+            "Nilai dari tipe data boolean adalah: " + mahasiswa + "\n");
+
+        System.out.println(
+            "========= Kumpulan Nilai =========");
+
+        System.out.println(
+            "Nilai Minimal char adalah: " + Character.MIN_VALUE);
+        System.out.println(
+            "Nilai Maksimal char adalah: " + Character.MAX_VALUE);
+
+        System.out.println(
+            "Nilai boolean true adalah: " + true);
+        System.out.println(
+            "Nilai boolean false adalah: " + false);
+
+        System.out.println(
+            "==================================");
+    }
+}
