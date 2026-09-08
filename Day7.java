9bea6adf96bf49ce24fbd9c2c6f80599e0ab520d
toyayotta-spend <toyayotta@gmail.com>
2026-09-08 17:37:34 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,14 @@
+public class Day7 {
+    public static void main(String[] args) {
+
+        String nama = "Artika";
+        String jurusan = "Teknik Informatika";
+        String kampus = "Universitas Sulawesi Barat";
+
+        System.out.println("Pengenalan Tipe Data String");
+        System.out.println("----------------------------");
+        System.out.println("Nama     : " + nama);
+        System.out.println("Jurusan  : " + jurusan);
+        System.out.println("Kampus   : " + kampus);
+    }
+}
