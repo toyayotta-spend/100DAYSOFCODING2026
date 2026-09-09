cb44a0543646ca1d60fc4980fbc0f77739e4d707
toyayotta-spend <toyayotta@gmail.com>
2026-09-09 19:10:59 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,17 @@
+public class Day8 {
+    public static void main(String[] args) {
+
+        // Deklarasi variabel
+        int umur;
+
+        // Inisialisasi variabel
+        umur = 18;
+
+        System.out.println("Umur awal: " + umur);
+
+        // Update variabel
+        umur = 19;
+
+        System.out.println("Umur setelah di-update: " + umur);
+    }
+}
