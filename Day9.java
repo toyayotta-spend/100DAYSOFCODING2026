77f9e1e3c989e4adf53c1ed6fe28b3e67a715638
toyayotta-spend <toyayotta@gmail.com>
2026-09-10 19:04:55 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,12 @@
+public class day9 {
+    public static void main(String[] args) {
+
+        final String NAMA = "Artika";
+        final String UNIVERSITAS = "Universitas Sulawesi Barat";
+        final double PI = 3.14;
+
+        System.out.println("Nama: " + NAMA);
+        System.out.println("Universitas: " + UNIVERSITAS);
+        System.out.println("Nilai PI: " + PI);
+    }
+}
