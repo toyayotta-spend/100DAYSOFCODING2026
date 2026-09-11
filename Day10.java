7538b91c556979edbfde5726ce52b25fabc00d40
toyayotta-spend <toyayotta@gmail.com>
2026-09-11 19:09:38 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,13 @@
+public class day10 {
+    public static void main(String[] args) {
+
+        System.out.println("=== Batas Minimal dan Maksimal Tipe Data ===");
+
+        System.out.println("Byte  : " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+        System.out.println("Short : " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+        System.out.println("Int   : " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+        System.out.println("Long  : " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+        System.out.println("Float : " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+        System.out.println("Double: " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+    }
+}
