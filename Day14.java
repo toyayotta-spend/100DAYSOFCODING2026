3ffb88e36ba15dfea3b03a22dea06350d94d1c0a
toyayotta-spend <toyayotta@gmail.com>
2026-09-15 20:27:34 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,12 @@
+public class day14 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 8;
+
+        int penjumlahan = a + b;
+        int pengurangan = a - b;
+
+        System.out.println("Hasil Penjumlahan: " + penjumlahan);
+        System.out.println("Hasil Pengurangan: " + pengurangan);
+    }
+}
