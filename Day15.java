4d36c0f3e072aa85d3d371fee620226d1c2d18a7
toyayotta-spend <toyayotta@gmail.com>
2026-09-16 18:11:18 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,22 @@
+public class day15 {
+
+    public static void main(String[] args) {
+
+        // deklarasi dan inisialisasi variable
+        int a = 20;
+        int b = 4;
+        int hasil, hasil2;
+
+        // operasi
+        hasil = a * b;
+        hasil2 = a / b;
+
+        System.out.println(
+            "Hasil perkalian dari a * b adalah: " +
+            hasil);
+
+        System.out.println(
+            "Hasil Pembagian dari a / b adalah: " +
+            hasil2);
+    }
+}
