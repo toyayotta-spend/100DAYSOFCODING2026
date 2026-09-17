7c572869eb15a114cccad87b4e71e8faee803c91
toyayotta-spend <toyayotta@gmail.com>
2026-09-17 19:51:46 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,13 @@
+public class Day16 {
+    public static void main(String[] args) {
+        
+        int angka1 = 17;
+        int angka2 = 5;
+        
+        int hasil = angka1 % angka2;
+        
+        System.out.println("Angka 1 = " + angka1);
+        System.out.println("Angka 2 = " + angka2);
+        System.out.println("Sisa bagi = " + hasil);
+    }
+}
