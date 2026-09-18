aaa40aede5a8d1756330cebd706591cb218534c4
toyayotta-spend <toyayotta@gmail.com>
2026-09-18 20:34:28 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,22 @@
+public class day17 {
+    public static void main(String[] args) {
+
+        int angka = 10;   // Operator =
+        System.out.println("Nilai awal: " + angka);
+
+        angka += 5;       // Operator +=
+        System.out.println("Setelah += 5: " + angka);
+
+        angka -= 3;       // Operator -=
+        System.out.println("Setelah -= 3: " + angka);
+
+        angka *= 2;       // Operator *=
+        System.out.println("Setelah *= 2: " + angka);
+
+        angka /= 4;       // Operator /=
+        System.out.println("Setelah /= 4: " + angka);
+
+        angka %= 3;       // Operator %=
+        System.out.println("Setelah %= 3: " + angka);
+    }
+}
