df6dd0b42acf346d629b5ee223aea7893fa8a1f4
toyayotta-spend <toyayotta@gmail.com>
2026-09-19 18:58:52 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,20 @@
+public class Day18 {
+    public static void main(String[] args) {
+
+        // Konversi Otomatis
+        // Dari tipe data kecil ke tipe data besar
+
+        int angkaInt = 100;
+
+        // int dikonversi otomatis menjadi long
+        long angkaLong = angkaInt;
+
+        // long dikonversi otomatis menjadi float
+        float angkaFloat = angkaLong;
+
+        // Menampilkan hasil
+        System.out.println("Nilai int   = " + angkaInt);
+        System.out.println("Nilai long  = " + angkaLong);
+        System.out.println("Nilai float = " + angkaFloat);
+    }
+}
