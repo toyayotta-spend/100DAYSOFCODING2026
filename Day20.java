1c62eabd00a932b0ceb94bf460a6568460986f35
toyayotta-spend <toyayotta@gmail.com>
2026-09-21 23:21:15 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,19 @@
+public class day20 {
+    public static void main(String[] args) {
+
+        int angka = 25;
+        double nilai = 85.5;
+        boolean benar = true;
+        char huruf = 'A';
+
+        String angkaString = String.valueOf(angka);
+        String nilaiString = String.valueOf(nilai);
+        String benarString = String.valueOf(benar);
+        String hurufString = String.valueOf(huruf);
+
+        System.out.println("Angka: " + angkaString);
+        System.out.println("Nilai: " + nilaiString);
+        System.out.println("Benar: " + benarString);
+        System.out.println("Huruf: " + hurufString);
+    }
+}
